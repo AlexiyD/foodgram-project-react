06fc5f12d031099fd4a5834c14734ead899aba18
Зубков Алексей <psi.alexeyzubkov@yandex.ru>
2023-05-20 16:16:08 +0300

    Vol4

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,5 +1,5 @@
-from django.db.models import Sum
 
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template.loader import get_template
